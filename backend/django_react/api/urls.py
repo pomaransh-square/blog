de379083a62dd499935ca1adce0133f408ce8d65
Nithelav <ghostinhosts@gmail.com>
2020-08-30 14:07:50 +0300

    Add token generating

--- backend/django_react/api/urls.py
+++ backend/django_react/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import CreateUserAPIView
+from .views import CreateUserAPIView, authenticate_user
  
 urlpatterns = [
     url(r'^create/$', CreateUserAPIView.as_view()),
+    url(r'^obtain_token/$', authenticate_user),
 ]
--- backend/django_react/api/views.py
+++ backend/django_react/api/views.py
@@ -2,7 +2,11 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_jwt.utils import jwt_payload_handler, jwt
 from .serializer import UserSerializer
+from .models import CustomUser
+from django.conf import settings
 
 class CreateUserAPIView(APIView):
     # Allow any user (authenticated or not) to access this url 
@@ -14,4 +18,31 @@ class CreateUserAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+ 
+    try:
+        email = request.data['email']
+        password = request.data['password']
+ 
+        user = CustomUser.objects.get(email=email, password=password)
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['token'] = token
+                return Response(user_details, status=status.HTTP_200_OK)
+ 
+            except Exception as e:
+                raise e
+        else:
+            res = {
+                'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'please provide a email and a password'}
+        return Response(res)
         
